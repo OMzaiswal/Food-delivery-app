cc7f0c6c61ad2da6dbac89f7a3715a8650c3996f
OM <omjaiswal2552@gmail.com>
2025-03-29 21:59:35 +0530

    cart..

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -5,6 +5,7 @@ import { assets, food_list } from "../assets/assets";
 export const Cart = () => {
 
     const [cartItems, setCartItems] = useRecoilState(cartState);
+    let subtotal = 0;
 
     const handleQuantityChange = (id: string, change: number) => {
         setCartItems((prev) => {
@@ -27,7 +28,6 @@ export const Cart = () => {
     }
     
     return <div className="mt-5 p-4">
-        <h2 className="text-2xl font-bold mb-4">Your cart items</h2>
 
         <div className="grid grid-cols-6 text-gray-700 text-xl font-semibold pb-2">
             <div>Items</div>
@@ -35,7 +35,7 @@ export const Cart = () => {
             <div>Price</div>
             <div>Quantity</div>
             <div>Total</div>
-            <div>Remove</div>
+            <div>Remove Item</div>
         </div>
         <hr />
         
@@ -43,12 +43,13 @@ export const Cart = () => {
             {Object.entries(cartItems).map(([id, quantity]) => {
                 const product = food_list.find((item) => item._id === id);
                 if (!product) return null;
+                subtotal += ( product.price * quantity )
 
                 return (
                     <div className="grid grid-cols-6 mt-4 text-xl items-center">
                         <img src={product.image} alt={product.name} className="w-16 h-16 object-cover rounded" />
                         <div>{product.name}</div>
-                        <div>{product.price}</div>
+                        <div>${product.price}</div>
                         <div className="flex space-x-2">
                             <img 
                                 src={assets.remove_icon_red} 
@@ -64,16 +65,46 @@ export const Cart = () => {
                             onClick={() => handleQuantityChange(id, +1)}
                         />
                         </div>
-                        <div>{product.price * quantity}</div>
+                        <div>${product.price * quantity}</div>
                         <button 
-                            className="text-start hover:text-red-400"
+                            className="text-start hover:text-red-400 hover:cursor-pointer"
                             onClick={() => handleRemove(id)}
-                            >X</button>
+                            >Remove</button>
                     </div>
                 )
             })}
         </div>
 
+        <h2 className="text-2xl font-bold mt-12 mb-3">Cart Total</h2>
+        <div className="flex flex-col-reverse md:grid md:grid-cols-2 md:space-x-12">
+            <div className="space-y-1">
+                <div className="flex justify-between text-lg text-gray-500">
+                    <p>Subtotal</p>
+                    <p>${subtotal}</p>
+                </div>
+                <div className="flex justify-between text-lg text-gray-500">
+                    <p>Delivery Fee</p>
+                    <p>$2</p>
+                </div>
+                <div className="flex justify-between text-lg text-gray-500">
+                    <p>Promo Discount</p>
+                    <p>$0</p>
+                </div>
+                <hr className="text-gray-400"/>
+                <div className="flex justify-between text-lg text-gray-500">
+                    <b>Total</b>
+                    <b>${subtotal + 2}</b>
+                </div>
+                <button className="p-2 bg-orange-400 rounded-md text-white mt-2">PROCEED TO CHECKOUT</button>
+            </div>
+            <div className="my-2">
+                <p className="text-lg">If you have a promo code, Enter it here</p>
+                <div className="mt-1">
+                    <input type="text" placeholder="promo code" className="px-6 py-3 bg-gray-200 rounded-md" />
+                    <button className="px-8 py-3 bg-black text-white rounded-md">Submit</button>
+                </div>
+            </div>
+        </div>
     </div>
 }
 
