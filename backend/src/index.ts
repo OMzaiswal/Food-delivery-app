0049fc5c34fd31753a00e46632fbbdf964ea52a8
OM <omjaiswal2552@gmail.com>
2025-04-05 09:09:36 +0530

    cart add and update for user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import adminRoutes from './routes/adminRoutes';
 import userRoute from './routes/userRoutes';
+import cartRoutes from './routes/cartRoutes';
+import { prisma } from './config/database';
 
 dotenv.config()
 
@@ -17,9 +19,20 @@ app.use(cors({
     credentials: true
 }))
 
+app.get('/foodList', (req, res) => {
+    try {
+        const foodList = prisma.foodItem.findMany();
+        res.json(foodList);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({message: "Internal server error"});
+    }
+})
+
 
 app.use('/admin', adminRoutes);
 app.use('/user', userRoute);
+app.use('/cart', cartRoutes);
 
 app.listen(3000, () => {
     console.log('App is listening at port 3000');
---
+++ backend/src/routes/cartRoutes.ts
@@ -0,0 +1,122 @@
+import express, { Request, Response } from 'express';
+import { authenticate, AuthorizeUser } from '../middlewares/authMiddleware';
+import { prisma } from '../config/database';
+
+
+const router = express.Router();
+
+router.use(authenticate, AuthorizeUser);
+
+// Get cart items
+router.get('/', async (req, res) => {
+    const userId = req.user?.id;
+
+    try {
+        const cart = await prisma.cart.findUnique({
+            where: {
+                userId
+            }, 
+            include: {
+                items: {
+                    include: {
+                        foodItem: true
+                    }
+                }
+            }
+        })
+        res.json(cart?.items || []);
+        return;
+    } catch(err) {
+        res.status(500).json({message: "Failed to fetch cart"});
+        return;
+    }
+})
+
+// Add item to cart
+router.post('/add', async (req, res) => {
+    const userId = req.user?.id;
+    const { foodItemId, quantity } = req.body;
+
+    if (!userId) {
+        res.status(201).json({message: "Unauthorized"});
+        return;
+    }
+
+    try {
+        let cart = await prisma.cart.findUnique({
+            where: { userId }
+        })
+        if (!cart) {
+            cart = await prisma.cart.create({
+                data: { userId }
+            })
+        }
+
+        const existingItem = await prisma.cartItem.findFirst({
+            where: {
+                cartId: cart.id,
+                foodItemId
+            }
+        });
+
+        if (existingItem) {
+            const updatedItem = await prisma.cartItem.update({
+                where: { id: existingItem.id },
+                data: { quantity: existingItem.quantity + quantity }
+            })
+            res.json(updatedItem);
+            return;
+        };
+
+        const newItem = await prisma.cartItem.create({
+            data: {
+                cartId: cart.id,
+                foodItemId,
+                quantity
+            }
+        })
+        res.json(newItem);
+        return;
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({message: "Failed to add to cart"});
+        return;
+    }
+})
+
+//  Update item quantity
+
+router.put('/update', async (req, res) => {
+    const userId = req.user?.id;
+    const { foodItemId, quantity } = req.body;
+
+    try {
+        const cart = await prisma.cart.findUnique({
+            where: { userId }
+        })
+        if(!cart) {
+            res.status(404).json({message: "Cart not found"});
+            return;
+        }
+
+        const item = await prisma.cartItem.findFirst({ where: { cartId: cart?.id, foodItemId }})
+        if (!item) {
+            res.status(404).json({message: "Item not in cart"});
+            return;
+        }
+
+        const updatedItem = await prisma.cartItem.update({
+            where: { id: item.id },
+            data: { quantity }
+        })
+
+        res.json(updatedItem);
+        return;
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({message: 'Faied to update the item'});
+    }
+})
+
+export default router;
