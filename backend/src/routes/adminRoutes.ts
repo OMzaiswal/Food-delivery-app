8e3007e49abd346527a0d316560437c12e07c7b2
OM <omjaiswal2552@gmail.com>
2025-04-09 13:28:29 +0530

    added order & order status update routes

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -44,21 +44,65 @@ router.post('/add-food', upload.single('image'), async (req, res) => {
                     imageUrl
                 }
             });
-            res.status(201).json({message: "Food item added", food })
+            res.status(201).json({message: "Food item added", food });
+            return;
         });
 
         blobStream.on('error', (err) => {
             res.status(500).json({message: "Upload failed", details: err.message });
+            return;
         });
     } catch(err) {
         res.status(500).json({ error: "Something went wrong", details: err });
+        return;
     }
 });
 
 
 
-router.get('/dashboard', authenticate, AuthorizeAdmin, (req, res) => {
+router.get('/orders', async (req, res) => {
+    try {
+        const orders = await prisma.order.findMany({
+            orderBy: { createdAt: 'desc' },
+            take: 20,
+            select: {
+                id: true,
+                totalPrice: true,
+                status: true,
+                createdAt: true
+            }
+        });
+        res.status(200).json(orders);
+        return;
+    } catch(err) {
+        res.status(500).json({message: "Failed to fetch orders"});
+        return;
+    }
+})
 
+router.patch('/order/:orderId/status', async (req, res) => {
+    const { orderId } = req.params;
+    const { newStatus } = req.body;
+
+    const allowedStatus = ['PENDING', 'CONFIRMED', 'PREPARING', 'OUT_FOR_DELIVERY', 'DELIVERED', 'CANCELLED']
+    if (!allowedStatus.includes(newStatus)) {
+        res.status(400).json({ message: 'Invalid status value'});
+        return;
+    }
+   
+    try {
+        const updatedOrder = await prisma.order.update({
+            where: { id: orderId },
+            data: { status: newStatus }
+        });
+
+        res.status(200).json({ message: "Order status updated successfully", updatedOrder });
+        return;
+
+    } catch (err) {
+        res.status(500).json({message: "Failed to update order status"});
+        return;
+    }
 })
 
 export default router;
