99333727419ef5616a7b4bf79e0a47ae4b4ffdd4
OM <omjaiswal2552@gmail.com>
2025-05-04 16:15:03 +0530

    modified AdminOrders

--- frontend/src/pages/admin/AdminOrders.tsx
+++ frontend/src/pages/admin/AdminOrders.tsx
@@ -5,12 +5,28 @@ import { toast } from "react-toastify";
 
 export const AdminOrders = () => {
 
-    const [orders, setOrders] = useState<any>([]);
+      const statusOptions = [
+        "PENDING",
+        "CONFIRMED",
+        "PREPARING",
+        "OUT_FOR_DELIVERY",
+        "DELIVERED",
+        "CANCELLED"
+      ];
+
+      type Order = {
+        id: string;
+        status: string;
+        totalPrice: string;
+        createdAt: string;
+      }
+
+    const [orders, setOrders] = useState<Order[]>([]);
 
     useEffect(() => {
         const fetchOrders = async () => {
             try {
-                const response = await api.get('/admin/orders');
+                const response = await api.get<Order[]>('/admin/orders');
                 if (response.status === 200) {
                     setOrders(response.data);
                 } else {
@@ -23,10 +39,29 @@ export const AdminOrders = () => {
         fetchOrders();
     }, [])
 
+    const handleStatusChange = async (orderId: string, newStatus: string) => {
+        try {
+            const res = await api.patch(`/admin/order/${orderId}/status`, { newStatus });
+
+            if (res.status === 200) {
+                setOrders((prev) => 
+                prev.map((order) => 
+                    order.id === orderId ? { ...order, status: newStatus } : order
+                ))
+                toast.success("Status updated successfully");
+            } else {
+                toast.warn('Failed to update status');
+            }
+        } catch(err) {
+            console.log('Your error: ',err);
+            toast.error('Error updating status');
+        }
+    }
+
     return <div className="mt-10">
         <h1 className="text-2xl text-gray-600 mb-4">Orders Page</h1>
         <div>
-            <div className="grid grid-cols-5 gap-4 text-sm font-semibold border border-gray-300 rounded-sm px-3 py-3 items-center">
+            <div className="grid grid-cols-5 gap-4 text-sm font-semibold border border-gray-300 rounded-sm px-3 py-3 items-center text-orange-400">
                 <p className="text-center">Order Id</p>
                 <p className="text-center">Total Price</p>
                 <p className="text-center">Status</p>
@@ -38,11 +73,21 @@ export const AdminOrders = () => {
                     <div key={order.id} 
                         className="grid grid-cols-5 gap-4 text-sm font-semibold border border-gray-300 rounded-sm px-3 py-3 items-center"
                         >
-                        <p>{order.id}</p>
-                        <p>{order.tota}</p>
-                        <p>{order.status}</p>
-                        <p>{order.createdAt}</p>
-                        <button>View Details</button>
+                        <p className="text-center truncate">{order.id}</p>
+                        <p className="text-center">${Number(order.totalPrice)}</p>
+                        <select
+                            className="border px-2 py-1"
+                            value={order.status} 
+                            onChange={(e) => handleStatusChange(order.id, e.target.value)}
+                        >
+                            {statusOptions.map((status) => (
+                                <option key={status} value={status}>
+                                    {status}
+                                </option>
+                            ))}
+                        </select>
+                        <p className="text-center">{new Date(order.createdAt).toString()}</p>
+                        <button className="text-center">View Details</button>
                     </div>
                 ))}
             </div>
