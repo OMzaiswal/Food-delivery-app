4af835caccee909366362cb41008a607956633b0
OMzaiswal <72777670+OMzaiswal@users.noreply.github.com>
2025-05-13 23:40:12 +0530

    Update PlaceOrder.tsx

--- frontend/src/pages/PlaceOrder.tsx
+++ frontend/src/pages/PlaceOrder.tsx
@@ -107,9 +107,9 @@ export const PlaceOrder = () => {
                     onClick={handleCheckout}
                 >PROCEED TO PAYMENT</button>
             </div>
-            <div className="text-red-500 text-md">
-               ( Note: please put card number 4242 4242 4242 4242 for successfull payment
-                for other fields, you can put any fake data )
+            <div className="text-red-500 text-md mt-2">
+               ( Note: please put card number 4242 4242 4242 4242 for successfull payment. 
+                For other fields, you can put any fake data )
             </div>
     </div>
     </div>
