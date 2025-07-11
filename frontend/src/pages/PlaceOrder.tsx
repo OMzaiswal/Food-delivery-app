4cf75ba0fa90b4f31e3274237a8411bac601c64d
OM <omjaiswal2552@gmail.com>
2025-07-11 12:15:39 +0530

    modified placeOrder.tsx

--- frontend/src/pages/PlaceOrder.tsx
+++ frontend/src/pages/PlaceOrder.tsx
@@ -18,7 +18,10 @@ export const PlaceOrder = () => {
     const [country, setCountry] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
 
+    const [loading, setLoading] = useState(false);
+
     const handleCheckout = async () => {
+        setLoading(true)
         try {
             const res = await api.post<{ url: string }>('/user/create-checkout-session', { 
                 amount,
@@ -40,6 +43,8 @@ export const PlaceOrder = () => {
         } catch (err) {
             console.log('Error while checkout', err);
             toast.error('Something went wrong, try again');
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -105,7 +110,16 @@ export const PlaceOrder = () => {
                 </div>
                 <button className="px-8 py-3 bg-orange-500 rounded-md text-white mt-2 hover:scale-105"
                     onClick={handleCheckout}
-                >PROCEED TO PAYMENT</button>
+                >
+                    {loading ? (
+                        <svg className="animate-spin h-5 w-5 mx-auto text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+                      </svg>
+                    ) : (
+                        'PROCEED TO PAYMENT'
+                    )}
+                </button>
             </div>
             <div className="text-red-500 text-md mt-2">
                ( Note: please put card number 4242 4242 4242 4242 for successfull payment. 
